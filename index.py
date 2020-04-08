0f5bdba7ef4a850a484dfffc41639b9a29a37666
Abigail Glenn <ag2461@msstate.edu>
2020-04-07 20:22:03 -0500

    upload of index,py

--- index.py
+++ index.py
@@ -1,75 +1,200 @@
-#cse 4283 assignment 2
+from browser import document
 
 
-def BMI():    #BMI calculator function
-    print("Body Mass Index Calculator: Select an option \n0:Calculate BMI \n1:exit")
-    while(1):
-        BMI_choice = input(": ")
-        if BMI_choice == "0":
-            print("please enter height")
-            height_ft = int(input("feet: "))
-            height_in = int(input("inches: "))
-            weight_lbs = int(input("please enter weight in pounds: "))
-            height_in = (height_ft * 12) + height_in
-            height_m = height_in *0.025
-            weight_kg = weight_lbs * 0.45
-            bmi = weight_kg / (height_m ** 2)
-            
-            if bmi <= 18.5:
-                category = "underweight"
-            elif bmi > 18.5 and bmi <= 24.9:
-                category = "normal weight"
-            elif bmi >= 25 and bmi <= 29.9:
-                category = "over weight"
-            elif bmi >= 30:
-                category = "obese"
-            print("BMI:",bmi, category)
-            BMI() #returns to allow for exit or to calculate again
-        elif BMI_choice == "1":
-            main() #returns to main menu 
+def events():
+  document['bmiCalculator'].bind('click', bmi)
+  document['retirementCalculator'].bind('click', retirement )
+  print('Set up all buttons')
 
-def retirement(): #retirement age calculator
-    print("Retirement Age Calculator: Select an option \n0:Claculate age \n1:exit")
-    while(1):
-        r_choice = input(":")
-        if r_choice == "0":
-            age = int(input("Please enter current age: "))
-            a_salary = int(input("Please enter annual salary: "))
-            per_savings = int(input("Please enter personal percentage saved: "))
-            goal = int(input("Please enter desired personal savings goal: "))
-            p_savings = (a_salary * per_savings)*.01
-            yearly_savings = (((p_savings * 35))*.01) + p_savings
-            total_savings = 0
-            while total_savings < goal:
-                total_savings = total_savings + yearly_savings
-                age += 1
-            if age >= 100: #life expectancy assumed to be less than 100
-                print("Savings goal can not be met.")
-            else:
-                print("Savings goal will be met at age",age)
+  document['feet'].bind('input', feet)
+  document['inches'].bind('input', inches)
+  document['weight'].bind('input', weight)
+  
+  document['currentAge'].bind('input', currentAge)
+  document['salary'].bind('input', salary_on_change)
+  document['percentSaved'].bind('input', percentSaved)
+  document['saveGoal'].bind('input', saveGoal)
+  print('Set up all event listeners')
 
-            
-            retirement() #returns to function menu
+  feet_on_change('')
+  inches_on_change('')
+  current_age_on_change('')
+  percent_saved_on_change('')
+  print("Ran slider's first feedback")
 
-        elif r_choice == "1":
-            main() #returns to main menu
-        
+# Helpers associated with getting user's BMI
+def run_bmi(event):
+  # Lead in with title
+  print('\nCalculating BMI')
+  # Expected input
+  height = get_height()
+  print(' Height: ' + str(height))
+
+  # Expected input
+  weight = get_weight()
+  print(' Weight: ' + str(weight))
+
+  if not(client.valid_bmi_values(height, weight)):
+    document['bmiResult'].innerHTML = ''
+    return
+
+  print('\n Results:')
+  calculate_bmi(height, weight)
+
+def get_height():
+  feet = int(document['feet'].value)
+  inches = float(document['inches'].value)
+  return client.get_height(feet, inches)
+
+def get_weight():
+  weight = document['weight'].value
+
+  (valid, weight) = client.valid_weight(weight)
+  if (valid):
+    return weight
+  return 'Invalid weight value'
+
+def calculate_bmi(height, weight):
+  (bmi, category) = client.get_bmi(height, weight)
+  output = 'Calculated BMI: ' + str(bmi) + '<br> BMI Category: ' + str(category)
+
+  print(output)
+  document['bmiResult'].innerHTML = output
+
+# Helper associated with getting user's estimated retirement age
+def run_retirement_calculator(event):
+  # Lead in with title
+  print('\nCalculating age of retirement')
+  
+  age = get_current_age()
+  print(' Age: ' + str(age))
+
+  salary = get_salary()
+  print(' Salary: ' + str(salary))
+
+  percent_saved = get_percent_saved()
+  print(' Percent Saved: ' + str(percent_saved))
+
+  save_goal = get_save_goal()
+  print(' Save goal: ' + str(save_goal))
+
+  if not(client.valid_retirement_values(age, salary, percent_saved, save_goal)):
+    document['retirementResult'].innerHTML = ''
+    return
+
+  print('\n Results')
+  calculate_retirement_age(age, salary, percent_saved, save_goal)
+
+def get_current_age():
+  return int(document['currentAge'].value)
+
+def get_salary():
+  salary = document['salary'].value
+
+  (valid, salary) = client.valid_salary(salary)
+  if (valid):
+    return salary
+  return 'Invalid salary value'
+
+def get_percent_saved():
+  return float(document['percentSaved'].value)
+
+def get_save_goal():
+  save_goal = document['saveGoal'].value
+
+  (valid, save_goal) = client.valid_save_goal(save_goal)
+
+  if (valid):
+    return save_goal
+  return 'Invalid save goal value'
+
+def calculate_retirement_age(age, salary, percent_saved, save_goal):
+  (met, age_met) = client.get_retirement_age(age, salary, percent_saved, save_goal)
+
+  output = 'Goal: '
+  if (met):
+    output = output + 'Met <br> Age: ' + str(age_met)
+  else:
+    output = output + 'Not met'
+
+  document['retirementResult'].innerHTML = output
 
 
-def main(): #main menu function
-    print("Welcome! Please select a function: \n0:Body Mass Index Calculator \n1:Retirement Calculator \n2:Exit")
+# User input BMI
+def feet(event):
+  value = document['feet'].value
+  print('- onChange: Feet ' + str(value))
+  document['feetLabel'].textContent = 'Feet: ' + str(value);
 
-    while(1):
-        choice = input(": ")
-        if choice == "0":
-            BMI()
-            break
-        elif choice == "1":
-            retirement()
-            break
-        elif choice == "2": #program can be exited here
-            break
-    print("Goodbye!")
+def inches(event):
+  value = document['inches'].value
+  print('- onChange: Inches ' + str(value))
+  document['inchesLabel'].textContent = 'Inches: ' + str(value);
+
+def weight(event):
+  value = document['weight'].value
+  (valid, value) = client.valid_weight(value)
+
+  if (valid):
+    document['weightWarning'].textContent = ''
+  else:
+    document['weightWarning'].textContent = 'Invalid weight value'
+
+#calculate bmi
+def get_height(feet, inches):
+  return (int(feet)*12) + float(inches)
+
+def get_bmi(height, weight):
+  bmi = (weight * 0.45) / ((height * 0.025)**2)
+  bmi = round(bmi,1)
+
+  category = ''
+  if (bmi < 18.5):
+    category = 'Underweight'
+  elif (bmi >= 18.5 and bmi <= 24.9):
+    category = 'Normal weight'
+  elif (bmi >= 25 and bmi <= 29.9):
+    category = 'Overweight'
+  else:
+    category = 'Obese'
+
+  return (bmi, category)
 
 
-main() 
+
+    #User input Retirement
+def currentAge(event):
+  value = document['currentAge'].value
+  document['currentAgeLabel'].textContent = 'Current age: ' + str(value);
+
+def salary(event):
+  value = document['salary'].value
+  (valid, value) = client.valid_salary(value)
+
+  if (valid):
+    document['salaryWarning'].textContent = ''
+  else:
+    document['salaryWarning'].textContent = 'Invalid salary value'
+
+def percentSaved(event):
+  value = document['percentSaved'].value
+  document['percentSavedLabel'].textContent = 'Percent Saved (%): ' + str(value) + '%';
+
+def saveGoal(event):
+  value = document['saveGoal'].value
+  (valid, value) = client.valid_save_goal(value)
+
+  if (valid):
+    document['saveGoalWarning'].textContent = ''
+  else:
+    document['saveGoalWarning'].textContent = 'Invalid save goal value
+    
+#calculate retirement
+def get_retirement_age(age, salary, percent_saved, save_goal):
+  percent_saved *= 1.35
+  years = save_goal / (salary * (percent_saved/100))
+  age_met = years + age
+  age_met = round(age_met)
+
+  met = age_met < 100
+  return (met, age_met)
